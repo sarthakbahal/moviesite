6895b8e90a2f46dfc9e1df9b1d442bbe16da5c6b
sarthakbahal <sarthakbahal.45@gmail.com>
2025-06-01 16:41:04 +0530

    EsLint issue fixed

--- movie-site/eslint.config.js
+++ movie-site/eslint.config.js
@@ -1,10 +1,8 @@
-import js from '@eslint/js'
 import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -20,10 +18,8 @@ export default [
       'react-refresh': reactRefresh
     },
     rules: {
-      ...js.configs.recommended.rules,
       ...reactPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true }
